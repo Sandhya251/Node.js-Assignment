4c6887357e4cb7664e5b383a69162714c2945107
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-05-20 23:22:29 +0530

    Use Railway-assigned port

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const schoolRoutes = require('./routes/schoolRoutes');
 app.use(express.json());
 app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
