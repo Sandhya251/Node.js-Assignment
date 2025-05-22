7725789617f3a1e84607b0e7efcce004a504be07
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-05-22 14:06:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const schoolRoutes = require('./routes/schoolRoutes');
 app.use(express.json());
 app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
