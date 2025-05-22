24ba98a04635812582439d39efbbe9537b2a818a
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-05-22 14:24:14 +0530

    Update server.js for Railway deployment

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.use(express.json());
 app.use('/', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+     console.log(`Server running on port ${PORT}`));
