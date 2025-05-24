5be1eef62bb121b9ff1bd9a77a500c735b09a11a
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-05-24 22:38:03 +0530

    Add db.js for MySQL connection

--- db.js
+++ db.js
@@ -1,18 +1,31 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// // connection.connect((err) => {
+// //   if (err) {
+// //     console.error('DB Connection Failed:', err);
+// //     return;
+// //   }
+// //   console.log('Connected to database');
+// // });
+// module.exports = pool.promise();
+// db.js
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('DB Connection Failed:', err);
-//     return;
-//   }
-//   console.log('Connected to database');
-// });
-module.exports = pool.promise();
+module.exports = pool;
